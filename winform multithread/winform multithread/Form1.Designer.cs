ee47f953b5cfc42fef860b91e46e7ed1db7c72d0
bargon3 <55709976+bargon3@users.noreply.github.com>
2019-11-27 22:59:08 +0200

    Add files via upload
    the current client gui with error management (without hint)

---
+++ winform multithread/winform multithread/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace winform_multithread
+{
+    partial class Form1
+    {
+        public System.Windows.Forms.ListBox listBox1;
+        public System.Windows.Forms.Button button1;
+        public System.Windows.Forms.TextBox textBox1;
+        public System.Windows.Forms.Label label1;
+
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.Enter_name = new System.Windows.Forms.Label();
+            this.My_name = new System.Windows.Forms.Label();
+            this.Cur_level = new System.Windows.Forms.Label();
+            this.My_level = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(7, 123);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(534, 304);
+            this.listBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.SystemColors.Info;
+            this.button1.Location = new System.Drawing.Point(401, 77);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 42);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "send";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button_clicked);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(11, 99);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(386, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 442);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // Enter_name
+            // 
+            this.Enter_name.AutoSize = true;
+            this.Enter_name.Font = new System.Drawing.Font("Monotype Corsiva", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Enter_name.Location = new System.Drawing.Point(23, 19);
+            this.Enter_name.Name = "Enter_name";
+            this.Enter_name.Size = new System.Drawing.Size(73, 28);
+            this.Enter_name.TabIndex = 5;
+            this.Enter_name.Text = "Name:";
+            // 
+            // My_name
+            // 
+            this.My_name.AutoSize = true;
+            this.My_name.Font = new System.Drawing.Font("Miriam Fixed", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.My_name.Location = new System.Drawing.Point(102, 23);
+            this.My_name.Name = "My_name";
+            this.My_name.Size = new System.Drawing.Size(0, 24);
+            this.My_name.TabIndex = 6;
+            // 
+            // Cur_level
+            // 
+            this.Cur_level.AutoSize = true;
+            this.Cur_level.Font = new System.Drawing.Font("Monotype Corsiva", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cur_level.Location = new System.Drawing.Point(251, 19);
+            this.Cur_level.Name = "Cur_level";
+            this.Cur_level.Size = new System.Drawing.Size(71, 28);
+            this.Cur_level.TabIndex = 7;
+            this.Cur_level.Text = "Level:";
+            // 
+            // My_level
+            // 
+            this.My_level.AutoSize = true;
+            this.My_level.Font = new System.Drawing.Font("Miriam Fixed", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.My_level.Location = new System.Drawing.Point(328, 23);
+            this.My_level.Name = "My_level";
+            this.My_level.Size = new System.Drawing.Size(0, 24);
+            this.My_level.TabIndex = 8;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.ClientSize = new System.Drawing.Size(552, 475);
+            this.Controls.Add(this.My_level);
+            this.Controls.Add(this.Cur_level);
+            this.Controls.Add(this.My_name);
+            this.Controls.Add(this.Enter_name);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label Enter_name;
+        private System.Windows.Forms.Label My_name;
+        private System.Windows.Forms.Label Cur_level;
+        private System.Windows.Forms.Label My_level;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ winform multithread/winform multithread/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using System.Threading;
+
+namespace winform_multithread
+{
+    public partial class Form1 : Form
+    {
+        TcpClient client; // socket client
+        StreamWriter sw;  // streamWriter
+        StreamReader sr;  // streamReader
+        char[] charArray = new char[100]; // read char array
+        int ticks;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            client = new TcpClient();
+            client.Connect(new IPEndPoint(IPAddress.Parse("192.168.1.30"), 56923));
+            sw = new StreamWriter(client.GetStream()); sw.AutoFlush = true;
+            sr = new StreamReader(client.GetStream());
+            label1.Text = "connection established";
+
+            listBox1.Items.Add("Please eneter your name");
+            
+            Console.WriteLine("ok");
+            timer1.Start();
+            backgroundWorker1.RunWorkerAsync(sr);
+        }
+
+        private void button_clicked(object sender, EventArgs e)
+        {
+            ticks = 0;
+            timer1.Start();
+            string msg = textBox1.Text;
+            listBox1.Items.Add("my msg :" + msg);
+
+            if (msg.Contains("History") )
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.Add("History");
+                sw.WriteLine("SubjHistory");
+                
+            }  
+            else if ( listBox1.Items[0].ToString().Contains ("Please eneter your name") )
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.Add("Which subject do you want to be testest on?");
+                My_name.Text = msg;
+            }
+            else
+                sw.WriteLine(My_level.Text+ "-" + listBox1.Items[1]+ "-" + textBox1.Text);
+
+            textBox1.Text = "";
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e) // receive msg
+        {
+            StreamReader sr = e.Argument as StreamReader; // get streamReader argument from runWorkerAsync
+            var data = "";
+            var readByteCount = 0;
+            string[] words; string subj; string level;string ques_num; string question;
+
+
+            do {
+
+                readByteCount = sr.Read(charArray, 0, charArray.Length);
+
+                if (readByteCount > 0) {
+
+                    data = new string(charArray, 0, readByteCount);
+
+                    //Invoke(new Action(() => listBox1.Items.Add("server: " + data)));
+                    if (data.Contains("Right") || data.Contains("Wrong"))
+                    {
+                        Invoke(new Action(() => listBox1.Items.Clear()));
+                        Invoke(new Action(() => listBox1.Items.Add(data+"...")));
+                    }
+
+                    else
+                    {    
+                        words = data.Split('-');
+                        subj = words[0];
+                        level = words[1];
+                        ques_num = words[2];
+                        question = words[3];
+
+                        Invoke(new Action(() => My_level.Text = subj + "-" + level));
+                        Invoke(new Action(() => listBox1.Items.Add(ques_num + question )));
+
+                        for (int i = 4; i < words.Length; i++)
+                        {
+                            Invoke(new Action(() => listBox1.Items.Add(" ")));
+                            Invoke(new Action(() => listBox1.Items.Add(words[i])));
+
+                        }
+    
+
+                    }
+
+
+
+                }
+                else Thread.Sleep(100);
+                Console.WriteLine(data);
+                }   
+            while (data != "bye");
+            Invoke(new Action(() => label1.Text = "connection terminated"));
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e) {
+            client.Close();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ticks++;
+            if (ticks==20)
+                {
+                    sw.WriteLine("20-seconds");
+                    listBox1.Items.Add("dude your time");
+                }
+        }
+    }
+}
